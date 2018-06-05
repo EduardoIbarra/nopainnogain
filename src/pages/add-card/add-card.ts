ab6ff4ffbb2b7d05c792d43df0424e3805044069
Miguel Hernández <miguelhdzch@gmail.com>
2018-06-05 01:01:18 -0500

    Adding Model on form

--- src/pages/add-card/add-card.ts
+++ src/pages/add-card/add-card.ts
@@ -10,6 +10,14 @@ declare var OpenPay: any;
 })
 export class AddCardPage {
 
+    CardData: any = {
+        Cardholder: null,
+        CardNumber: null,
+        CCV: null,
+        ExpMonth: null,
+        ExpYear: null
+    };
+
     constructor(public navCtrl: NavController, public navParams: NavParams) {
     }
 
@@ -18,10 +26,29 @@ export class AddCardPage {
     }
 
     addCard() {
-        let form = document.getElementById('customer-form');
-        console.log(form);
+        // let form = document.getElementById('customer-form');
+        // console.log(form);
+        //
 
-        OpenPay.token.extractFormAndCreate('customer-form', success, error);
+        let form: any = `
+            <form id="customer-form">
+                <input type="hidden" name="token_id" id="token_id"/>
+   
+                <label>Nombre</label>
+                <input type="text" size="20" autocomplete="off"  value="${this.CardData.Cardholder}" data-openpay-card="holder_name"/>
+
+                <input type="text" size="20" autocomplete="off"  value="${this.CardData.CardNumber}" data-openpay-card="card_number"/>
+                <input type="text" size="4" autocomplete="off"  value="${this.CardData.CCV}" data-openpay-card="cvv2"/>
+        
+   
+                <input type="text" size="2" value="${this.CardData.ExpMonth}" data-openpay-card="expiration_month"/> 
+                <input type="text" size="2" value="${this.CardData.ExpYear}" data-openpay-card="expiration_year"/>
+           
+            </form>
+        
+        `;
+
+        OpenPay.token.extractFormAndCreate(form, success, error);
 
         function success(data) {
             console.log(data);
