fef521a0e322f3173707983c57aaf3d27fd7d59f
Miguel Hernandez <miguel@miguel.gateway.huawei.net>
2018-06-08 00:30:35 -0500

    Fix card select item

--- src/pages/gym-purchase/gym-purchase.ts
+++ src/pages/gym-purchase/gym-purchase.ts
@@ -7,43 +7,50 @@ declare var OpenPay: any;
 
 @IonicPage()
 @Component({
-    selector: 'page-gym-purchase',
-    templateUrl: 'gym-purchase.html',
+  selector: 'page-gym-purchase',
+  templateUrl: 'gym-purchase.html',
 })
 export class GymPurchasePage {
 
-    isPurchaseDone: boolean = false;
-    cards: any = [];
-    selectedCard: any;
+  isPurchaseDone: boolean = false;
+  cards: any = [];
+  selectedCard: any;
 
-    // openpay = new this.sharedService.OpenPay('mrtezzirtht6piewm54o', 'pk_c0a63b5356524d2095a0df7172965ed9');
+  // openpay = new this.sharedService.OpenPay('mrtezzirtht6piewm54o', 'pk_c0a63b5356524d2095a0df7172965ed9');
 
-    constructor(public navCtrl: NavController,
-                public navParams: NavParams,
-                public loadingService: LoadingService,
-                public sharedService: SharedService,) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public loadingService: LoadingService,
+              public sharedService: SharedService,) {
 
+  }
+
+  purchase() {
+    this.loadingService.presentLoading();
+    console.log(this.selectedCard);
+    setTimeout(() => {
+      // this.isPurchaseDone = true;
+      this.loadingService.dismiss();
+    }, 1500)
+  }
+
+  addCardPage() {
+    this.navCtrl.push('AddCardPage')
+  }
+
+  ionViewWillEnter() {
+    if (this.sharedService.UserData.Cards) {
+      this.cards = this.sharedService.UserData.Cards;
+      this.cards.forEach((c)=>{
+        c.card.brand = c.card.brand.toUpperCase();
+      });
+
+      this.selectedCard = this.cards[0];
     }
+    console.log(this.cards);
+  }
 
-    purchase() {
-        this.loadingService.presentLoading();
-        console.log(this.selectedCard);
-        setTimeout(() => {
-            this.isPurchaseDone = true;
-            this.loadingService.dismiss();
-        }, 1500)
-    }
-
-    addCardPage() {
-        this.navCtrl.push('AddCardPage')
-    }
-
-    ionViewWillEnter() {
-        if (this.sharedService.UserData.Cards) {
-            this.cards = this.sharedService.UserData.Cards;
-            this.selectedCard = this.cards[0];
-        }
-        console.log(this.cards);
-    }
-
+  selectedCardOption(c) {
+    this.selectedCard = c;
+  }
 }
